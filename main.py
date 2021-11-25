5176fe6a5fd0f0b026b5521bea1860e154ca56a9
Sennett Lau <laub1199@gmail.com>
2021-11-25 18:09:58 +0800

    debug

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def main():
     dp = updater.dispatcher
 
     # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start, filters=~Filters.group)))
+    dp.add_handler(CommandHandler("start", start, filters=~Filters.group))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("source", source, filters=~Filters.group))
 
