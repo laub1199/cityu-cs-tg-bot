8a9750fe5544461b25d9c67f56462bcfc68517ee
Sennett Lau <laub1199@gmail.com>
2021-11-26 21:14:43 +0800

    add back help function

--- main.py
+++ main.py
@@ -91,6 +91,14 @@ def fileHandler(update, context):
     context.bot.sendDocument(chat_id=query.message.chat.id, document=r.content, filename=filename)
     print(document)
 
+def help(update, context):
+    url = helpers.create_deep_linked_url(context.bot.get_me().username, 'city-cs')
+    keyboard = InlineKeyboardMarkup.from_button(
+        InlineKeyboardButton(text='Get Resource here!', url=url)
+    )
+
+    update.message.reply_text("Heyyy", reply_markup=keyboard)
+
 def updatelog(update, context):
     chat_id=update.message.chat.id
     txt = open("update_log.txt", "r").read()
