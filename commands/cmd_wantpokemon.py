3ef2d86990ed94d37ce79c67f79d387e086cbdfe
Sennett Lau <laub1199@gmail.com>
2021-11-29 22:17:31 +0800

    bugfix log

--- commands/cmd_wantpokemon.py
+++ commands/cmd_wantpokemon.py
@@ -6,12 +6,14 @@ def wantpokemon(update, context):
     client = pokepy.V2Client()
     poke_id = random.randrange(897) + 1
     poke = client.get_pokemon(poke_id)
+    print(poke.types)
+    print(poke.types[0])
     indefinite = 'an' if poke.types[0].type.name in ['a', 'e', 'i', 'o', 'u'] else 'a'
     text = 'This is {},\n {} '.format(poke.name, indefinite)
-    for index, types in enumerate(poke.types):
+    for index, poketype in enumerate(poke.types):
         if index > 0:
             text += ' & '
-        text += types.type.name
+        text += poketype.type.name
     text += ' pokémon!'
     poke_id = str(poke_id).zfill(3)
     photo_url = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/{}.png'.format(poke_id)
