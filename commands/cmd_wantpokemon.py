704b526ead1e2f11a1951cd658cdc22fa85aeab4
Sennett Lau <laub1199@gmail.com>
2021-11-29 22:26:07 +0800

    hide pokemon type

--- commands/cmd_wantpokemon.py
+++ commands/cmd_wantpokemon.py
@@ -3,23 +3,17 @@ import random
 
 
 def wantpokemon(update, context):
-    print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
     client = pokepy.V2Client()
-    print('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
     poke_id = random.randrange(897) + 1
-    print('cccccccccccccccccccccccccccccccc')
     poke = client.get_pokemon(poke_id)
-    print('dddddddddddddddddddddddddddddddd')
-    print(poke)
-    print('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
-    print(poke.types[0])
-    indefinite = 'an' if poke.types[0].type.name in ['a', 'e', 'i', 'o', 'u'] else 'a'
-    text = 'This is {},\n {} '.format(poke.name, indefinite)
-    for index, poketype in enumerate(poke.types):
-        if index > 0:
-            text += ' & '
-        text += poketype.type.name
-    text += ' pokémon!'
+    # indefinite = 'an' if poke.types[0].type.name in ['a', 'e', 'i', 'o', 'u'] else 'a'
+    text = 'This is {}!'.format(poke.name)
+    # text = 'This is {},\n {} '.format(poke.name, indefinite)
+    # for index, poketype in enumerate(poke.types):
+    #     if index > 0:
+    #         text += ' & '
+    #     text += poketype.type.name
+    #text += ' pokémon!'
     poke_id = str(poke_id).zfill(3)
     photo_url = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/{}.png'.format(poke_id)
     context.bot.send_photo(chat_id=update.message.chat.id, photo=photo_url, caption=text)
