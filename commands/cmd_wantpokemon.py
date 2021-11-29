e5f550899e86d09bf217eacd255258beb1d9de33
Sennett Lau <laub1199@gmail.com>
2021-11-29 22:52:05 +0800

    poke info bugfix

--- commands/cmd_wantpokemon.py
+++ commands/cmd_wantpokemon.py
@@ -1,21 +1,19 @@
-import pokepy
+import requests
+import json
 import random
 
 
 def wantpokemon(update, context):
-    client = pokepy.V2Client()
     poke_id = random.randrange(897) + 1
-    poke = client.get_pokemon(poke_id)
-    print(dir(poke))
-    text = ''
-    # indefinite = 'an' if poke.types[0].type.name in ['a', 'e', 'i', 'o', 'u'] else 'a'
-    # text = 'This is {}!'.format(poke.name)
-    # text = 'This is {},\n {} '.format(poke.name, indefinite)
-    # for index, poketype in enumerate(poke.types):
-    #     if index > 0:
-    #         text += ' & '
-    #     text += poketype.type.name
-    #text += ' pokémon!'
+    r = requests.get('https://pokeapi.co/api/v2/pokemon/{}/'.format(poke_id))
+    poke = json.loads(r.content)
+    indefinite = 'an' if poke['types'][0]['type']['name'] in ['a', 'e', 'i', 'o', 'u'] else 'a'
+    text = 'This is {},\n {} '.format(poke['name'], indefinite)
+    for index, poketype in enumerate(poke['types']):
+        if index > 0:
+            text += ' & '
+        text += poketype['type']['name']
+    text += ' pokémon!'
     poke_id = str(poke_id).zfill(3)
     photo_url = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/{}.png'.format(poke_id)
     context.bot.send_photo(chat_id=update.message.chat.id, photo=photo_url, caption=text)
