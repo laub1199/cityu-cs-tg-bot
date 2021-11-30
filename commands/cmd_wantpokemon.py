f0aec523cfd2c4c568d6e2ad8f6e267c9ea58185
Sennett Lau <laub1199@gmail.com>
2021-11-30 20:26:53 +0800

    bugfix indefinite

--- commands/cmd_wantpokemon.py
+++ commands/cmd_wantpokemon.py
@@ -7,7 +7,7 @@ def wantpokemon(update, context):
     poke_id = random.randrange(897) + 1
     r = requests.get('https://pokeapi.co/api/v2/pokemon/{}/'.format(poke_id))
     poke = json.loads(r.content)
-    indefinite = 'an' if poke['types'][0]['type']['name'] in ['a', 'e', 'i', 'o', 'u'] else 'a'
+    indefinite = 'an' if poke['types'][0]['type']['name'][0] in ['a', 'e', 'i', 'o', 'u'] else 'a'
     text = 'This is {},\n {} '.format(poke['name'], indefinite)
     for index, poketype in enumerate(poke['types']):
         if index > 0:
