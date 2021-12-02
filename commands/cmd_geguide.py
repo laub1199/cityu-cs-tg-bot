076f10c2a337b7923ced5ed9eadc2f5fb46be9ce
Sennett Lau <laub1199@gmail.com>
2021-12-03 02:43:02 +0800

    minor syntax

--- commands/cmd_geguide.py
+++ commands/cmd_geguide.py
@@ -2,10 +2,10 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 def geguide(update, context):
-    if (not context.args):
+    if not context.args:
         update.message.reply_text("Course code missing, e.g. /geguide ge1401")
-    elif (len(context.args) > 1):
-        update.message.reply_text("Only 1 couse code is required, e.g. /geguide ge1401")
+    elif len(context.args) > 1:
+        update.message.reply_text("Only 1 course code is required, e.g. /geguide ge1401")
     else:
         url = 'http://cityuge.swiftzer.net/courses/' + str(context.args[0]).lower()
         text = 'Check out ' + str(context.args[0]).upper() + '!'
