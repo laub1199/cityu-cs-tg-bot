cfd0ad1c5e0d4ae6a4399abf0c9facfc541acf99
Sennett Lau <laub1199@gmail.com>
2022-01-23 01:20:16 +0800

    Caption too long bugfix

--- commands/cmd_nhk.py
+++ commands/cmd_nhk.py
@@ -10,4 +10,4 @@ def nhk(update, context):
     keyboard = InlineKeyboardMarkup.from_button(
         InlineKeyboardButton(text=text, url=url)
     )
-    context.bot.send_photo(chat_id=update.message.chat.id, photo=profile['photo_url'], reply_markup=keyboard, caption=profile['caption'])
+    context.bot.send_photo(chat_id=update.message.chat.id, photo=profile['photo_url'], reply_markup=keyboard, caption='NFT與區塊鏈資訊平台﹐教授相關知識﹐致力推廣香港出品嘅非同質化代幤﹐介紹有實際應用嘅project')
